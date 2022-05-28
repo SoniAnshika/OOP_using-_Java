d16795b816cb043d6f2f740229ff6f33faeb6188
Anshika Soni <91690050+SoniAnshika@users.noreply.github.com>
2022-05-28 12:12:00 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+class Mother
+{
+    int x;
+    
+    void show()
+    {
+        System.out.println("Mother Class Function");
+    }
+}
+
+class child extends Mother
+{
+    
+}
+
+class Application
+{
+    public static void main(String args[])
+    {
+        /*Mother object calling mother function.*/
+        Mother m=new Mother();
+        m.show();
+        /*Child object calling mother function. */
+        child c=new child();
+        c.show();
+    }
+}
---
+++ Q3 (1).java
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+class Mother
+{
+    int x;
+    
+    void show()
+    {
+        System.out.println("Mother Class Function");
+    }
+}
+
+class child extends Mother
+{
+    void show()
+    {
+        System.out.println("Child Class Function");
+    }
+    
+}
+
+class Application
+{
+    public static void main(String args[])
+    {
+        /*Mother object calling mother function.*/
+        Mother m=new Mother();
+        m.show();
+        /*Child reference variable calling mother function. */
+        child c=new child();
+         c.show();
+        /*Calling object of child calss with the help of reference variable of Mother class. */
+        Mother m1=new child();
+        m1.show();
+       
+    }
+}
